a1f69cc249beb9100842b74899cae6040c15a697
Havocrazer <92991759+Havocrazer@users.noreply.github.com>
2023-12-21 14:04:40 +0700

    feat: add finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  let [greetingMessage, setGreetingMessage] = useState("Greeting Message");
+
+  function handleThaiClick() {
+    setGreetingMessage("สวัสดี!");
+  }
+
+  function handleEnglishClick() {
+    setGreetingMessage("Hi!");
+  }
+
+  function handleChineseClick() {
+    setGreetingMessage("你好!");
+  }
+
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{greetingMessage}</div>
       <div className="buttons">
-        <button>สวัสดี!</button>
-        <button>Hi!</button>
-        <button>你好!</button>
+        <button onClick={handleThaiClick}>สวัสดี!</button>
+        <button onClick={handleEnglishClick}>Hi!</button>
+        <button onClick={handleChineseClick}>你好!</button>
       </div>
     </div>
   );
